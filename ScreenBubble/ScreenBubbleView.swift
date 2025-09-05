e56105c23e5288ce0c7a62cd0fa66a6425cebc6f
Fugui <lacklock@gmail.com>
2025-09-05 18:05:42 +0800

    增加菜单切换效果

--- ScreenBubble/ScreenBubbleView.swift
+++ ScreenBubble/ScreenBubbleView.swift
@@ -15,26 +15,69 @@ enum BubbleState {
     case menu
 }
 
+enum BubbleGestureEvent {
+    case tap
+    case panStart
+    case panEnd
+}
+
+class BubbleStateManager: ObservableObject {
+    @Published var state: BubbleState = .normal
+    private var gestureEvent: BubbleGestureEvent? = nil
+
+    func updateEvent(_ event: BubbleGestureEvent) {
+        gestureEvent = event
+    }
+    
+    func toggleMenuState() {
+        if state == .menu {
+            self.state = .normal
+        } else {
+            self.state = .menu
+        }
+    }
+}
+
 /// 管理悬浮球交互和 UI
 struct ScreenBubbleView: View {
-    @State private var selectedState: BubbleState = .normal
+    @ObservedObject var stateManager: BubbleStateManager
 
     var body: some View {
-        BubbleView(state: selectedState)
+        if stateManager.state == .menu {
+            BubbleMenuView()
+                .transition(.blurReplace)
+        } else {
+            BubbleView(state: stateManager.state)
+        }
     }
 }
 
 struct ScreenBubblePreview: View {
-    
+    @StateObject private var stateManager = BubbleStateManager()
+
     var body: some View {
         ZStack {
-            Image(.bgPhone)
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
-            VStack {
-                ScreenBubbleView()
+            Color.clear.background {
+                Image(.bgPhone)
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
             }
+            VStack {
+                Spacer()
+                ScreenBubbleView(stateManager: stateManager)
+                Spacer()
+                HStack {
+                    Button("菜单切换") {
+                        withAnimation {
+                            stateManager.toggleMenuState()
+                        }
+                    }
+                    .buttonStyle(.bordered)
+                }
+            }
+            .padding(.bottom, 16)
+            .foregroundStyle(.white)
         }
     }
 }
